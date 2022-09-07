c0566439fd9b87f936c80aae402176aaf66adbdf
Curtis Wang <curtis.wang10@gmail.com>
2022-09-06 18:30:25 -0700

    push changes after pair programming W1D1 for getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+// I - className: string, documentElement: document.body: object
+// O - array of dom elements that contain className
+// C - use recursion
+// E -
+var getElementsByClassName = function(className, documentElement) {
+  // define result as empty array
+  var result = [];
+
+  // check if documentElement is undefined
+  if (!documentElement) {
+    // set documentElement equal to document.body
+    documentElement = document.body;
+  }
+
+  // check whether documentElement contains className
+  if (documentElement.classList && documentElement.classList.contains(className)) {
+    // push document element to result array
+    result.push(documentElement);
+  // otherwise
+  }
+
+  for (var i = 0; i < documentElement.childNodes.length; i++) {
+    result.concat(getElementsByClassName(className, documentElement.childNodes[i]));
+  }
+    // do a for loop for every childNodes of documentelement
+       // childNode has three nodes type --- text/ children/comment nodes, only children can have classname so we need to check if e
+      // if current childNode contains className
+        // call getElementsByClassName(current element)
+
+  // return result
+  console.log(result);
+  return result;
 };
+
+getElementsByClassName();
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,45 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+//I-- any primitive type (string, boolean, number, array, object, function, null, undefined)
+//O-- string version of input data;
+//c-- N/A
+//e-- function, null, undefined
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // if it is null, return null with string quotations;
+  if ( obj === null) {
+    return 'null';
+  }
+  // if it is a string, return string with string quotations;
+  if ( typeof obj === 'string') {
+    return '"' + obj + '"';
+  // if it is a boolean, return boolean with string quotations;
+  } else if (typeof obj === 'boolean') {
+    return String(obj);
+  // if it is a number, return number with string quotations;
+  } else if (typeof obj === 'number') {
+    return String(obj);
+  // if it is an array, return [ with string quotation and iterate each array element(do recursion at this point) and ] with string quotations;
+    // if there is function/undefined as an array element, stringify function should return string null;
+  } else if (Array.isArray(obj)) {
+    var res = [];
+    for ( var i = 0; i < obj.length; i++) {
+      if ( typeof obj[i] === 'function' || obj[i] === undefined) {
+        res.push('null');
+      }
+      res.push(stringifyJSON(obj[i]));
+    }
+    var arrStr = res.join(',');
+    return '[' + arrStr + ']';
+  // if it is an object, return { with string quotations and iterate Object properties with string key : and string value and } with string quotations}
+    //   if there is function/undefined as an object value, stringify function should not return;
+  }
+
+
+
+
+  // if it is pure function, return undefined;
+
+  // if it is undefined, return null;
 };
+// console.log(!null);
